4f147e245a8511e4d96348d6b14558fd0430dea9
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2022-05-30 00:09:57 +0800

    Add ColorIcon static method
    - preparing to create a color icon

--- Code/src/minesweeper/UI.java
+++ Code/src/minesweeper/UI.java
@@ -423,4 +423,35 @@ public class UI extends JFrame {
     }
     // ------------------------------------------------------------------------//
 
+    private static class ColorIcon implements Icon {
+
+        private final int size;
+        private Color color;
+
+        public ColorIcon(int size, Color color) {
+            this.size = size;
+            this.color = color;
+        }
+
+        @Override
+        public void paintIcon(Component c, Graphics g, int x, int y) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setColor(color);
+            g2d.fillRect(0, 0, size, size);
+        }
+
+        @Override
+        public int getIconWidth() {
+            return size;
+        }
+
+        @Override
+        public int getIconHeight() {
+            return size;
+        }
+    }
+
 }
