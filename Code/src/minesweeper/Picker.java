20d2979af7d0d45fd65eec325c2a836916ad065d
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2022-06-05 21:12:14 +0800

    Update on Picker class
    - Create CustomColorPicker class to add 2 buttons to it
    - Create another 2 contructor to create specific picker mode
    - Add getter to imagePicker, colorPicker and pickerState
    - Add setter to pickerState
    - Add constant COLOR_PICKER and IMAGE_PICKER
    - Picker panel is store in HashMap mapped to pickerState mode
    - Picker is now a JDialog instead of JFrame

--- Code/src/minesweeper/Picker.java
+++ Code/src/minesweeper/Picker.java
@@ -1,46 +1,124 @@
 package minesweeper;
 
-import java.awt.event.ItemListener;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import java.awt.event.ItemEvent;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Container;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JRadioButton;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JColorChooser;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 
-public class Picker extends JFrame implements ItemListener {
-    private String pickerState;
+import javax.imageio.ImageIO;
 
-    private int res;
-    private int panelWidth = 620;
-    private int panelHeight = 325;
+public class Picker extends JDialog {
+    public static final String COLOR_PICKER = "color";
+    public static final String IMAGE_PICKER = "image";
+
+    private String pickerState;
+    private JFileChooser imagePicker;
+    private CustomColorPicker colorPicker;
 
     private JPanel upperPanel;
     private JPanel lowerPanel;
-
-    private JFileChooser imagePicker;
-
-    private JColorChooser colorPicker;
-
+    private ButtonGroup selectionType;
+    private HashMap<String, Container> mode;
     private ArrayList<JRadioButton> selectionButtons;
 
-    private ButtonGroup selectionType;
+    public Picker(JFrame parent) {
+        super(parent, "Resource Picker");
+        initPicker(COLOR_PICKER);
 
-    public Picker() {
-        // super(parent, "Resource Picker");
-        pickerState = "image";
+        addSelectionType(COLOR_PICKER, colorPicker);
+        addSelectionType(IMAGE_PICKER, imagePicker);
+
+        changePickerPanel();
+
+    }
+
+    public Picker(JFrame parent, String mode) {
+        super(parent, "Resource Picker");
+        if (mode.equals(COLOR_PICKER)) {
+            initPicker(COLOR_PICKER);
+            addSelectionType(COLOR_PICKER, colorPicker);
+        } else if (mode.equals(IMAGE_PICKER)) {
+            initPicker(IMAGE_PICKER);
+            addSelectionType(IMAGE_PICKER, imagePicker);
+        }
+        changePickerPanel();
+    }
+
+    public Picker(JFrame parent, String[] modes) {
+        super(parent, "Resource Picker");
+        initPicker(modes[0]);
+        for (String mode : modes) {
+            switch (mode) {
+                case COLOR_PICKER:
+                    addSelectionType(COLOR_PICKER, colorPicker);
+                    break;
+                case IMAGE_PICKER:
+                    addSelectionType(IMAGE_PICKER, imagePicker);
+                    break;
+            }
+        }
+        changePickerPanel();
+    }
+
+    public void addSelectionType(String selectionType, Container selectionPanel) {
+        JRadioButton button = new JRadioButton(selectionType, pickerState.equalsIgnoreCase(selectionType));
+        button.addItemListener((ItemEvent e) -> {
+            if (e.getStateChange() == ItemEvent.SELECTED && !pickerState.equalsIgnoreCase(selectionType)) {
+                pickerState = selectionType.toLowerCase();
+                changePickerPanel();
+            }
+        });
+        mode.put(selectionType.toLowerCase(), selectionPanel);
+        selectionButtons.add(button);
+        this.selectionType.add(button);
+        upperPanel.add(button);
+    }
+
+    public JFileChooser getImagePicker() {
+        return imagePicker;
+    }
+
+    public CustomColorPicker getColorPicker() {
+        return colorPicker;
+    }
+
+    public String getPickerState() {
+        return pickerState;
+    }
+
+    public void setPickerState(String state) {
+        pickerState = state;
+        changePickerPanel();
+    }
+
+    private void changePickerPanel() {
+        lowerPanel.removeAll();
+        lowerPanel.add(mode.get(pickerState));
+        lowerPanel.revalidate();
+        lowerPanel.repaint();
+        pack();
+    }
+
+    private void initPicker(String initialMode) {
+        pickerState = initialMode;
+        mode = new HashMap<String, Container>();
 
         TitledBorder selectionBorder = BorderFactory.createTitledBorder("Type: ");
         selectionBorder.setTitleJustification(TitledBorder.LEFT);
@@ -52,62 +130,42 @@ public class Picker extends JFrame implements ItemListener {
         selectionType = new ButtonGroup();
 
         lowerPanel = new JPanel();
-        Dimension s = new Dimension(panelWidth, panelHeight);
-        lowerPanel.setPreferredSize(s);
 
-        colorPicker = new JColorChooser();
+        colorPicker = new CustomColorPicker();
 
-        // FIXME: Browse item then preview image under it. Only if user press browse,
-        // then we will open up imagePicker.showOpenDialog();
         imagePicker = new JFileChooser();
         imagePicker.setFileSelectionMode(JFileChooser.FILES_ONLY);
         imagePicker.setFileFilter(new FileNameExtensionFilter("Image Files", ImageIO.getReaderFileSuffixes()));
-        // ------------------------------- //
-
-        changePickerPanel();
 
         setLayout(new BorderLayout());
         add(upperPanel, BorderLayout.NORTH);
         add(lowerPanel, BorderLayout.CENTER);
+        pack();
+    }
+}
+
+class CustomColorPicker extends JColorChooser {
+    private JButton confirmButton;
+    private JButton cancelButton;
+
+    CustomColorPicker() {
+        super();
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        confirmButton = new JButton("Confirm");
+        cancelButton = new JButton("Cancel");
+
+        buttonPanel.add(confirmButton);
+        buttonPanel.add(cancelButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
-    public void addSelectionType(String selectionType) {
-        JRadioButton button = new JRadioButton(selectionType, pickerState.equalsIgnoreCase(selectionType));
-        button.addItemListener((ItemEvent e) -> {
-            if (e.getStateChange() == ItemEvent.SELECTED && !pickerState.equalsIgnoreCase(selectionType)) {
-                pickerState = selectionType.toLowerCase();
-                changePickerPanel();
-            }
-        });
-        selectionButtons.add(button);
-        this.selectionType.add(button);
-        upperPanel.add(button);
+    public JButton getConfirmButton() {
+        return confirmButton;
     }
 
-    private void changePickerPanel() {
-        lowerPanel.removeAll();
-        if (pickerState.equalsIgnoreCase("color")) {
-            lowerPanel.add(colorPicker);
-        } else if (pickerState.equalsIgnoreCase("image")) {
-            lowerPanel.add(imagePicker);
-        }
-        lowerPanel.revalidate();
-        lowerPanel.repaint();
-    }
-
-    public static void main(String[] args) {
-        Picker p = new Picker();
-
-        p.addSelectionType("color");
-        p.addSelectionType("image");
-
-        p.pack();
-        p.setResizable(false);
-        p.setVisible(true);
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-
+    public JButton getCancelButton() {
+        return cancelButton;
     }
 }
